a45daeaf7ecfc31cf05e8af96f69db3b7822073e
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-08-20 16:22:14 -0400

    modifired index.js

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const port = 1000;
+
+const server = require("./server");
+
+server.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+})
