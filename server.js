1be1ea618e861e6c8dadd200eafdadec8ff15efa
Paul H Menard Jr <clmonitor2013@gmail.com>
2020-08-20 19:03:49 -0400

    removed err param

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const projectsRouter = require("./routers/projectsRouter");
 
 const server = express();
 
-server.use(express.json);
+server.use(express.json());
 
 server.use("/myapi/actions", actionsRouter);
 server.use("/myapi/projects", projectsRouter);
 
-server.use((err, req, res, next) =>{
+server.use((req, res, next) =>{
     res.status(500).json({message: "Internal server error."})
 })
 
